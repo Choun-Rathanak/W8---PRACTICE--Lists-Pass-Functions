d73819c4c9cc98b2d4b85b7419fa678c3f617093
Choun Rathanak <Nak4131@gmail.com>
2025-11-28 08:49:10 +0700

    fix already

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:w8_practice_list_pass_fuction/EXERCISE-4/database/jokes.dart';
-import '../ui/screens/welcome_screen.dart';
+import '../../EXERCISE-4/database/jokes.dart';
 
 Color appColor = Colors.green[300] as Color;
 
@@ -45,6 +44,66 @@ class _HomeScreenState extends State<HomeScreen> {
         favoriteJokeId: favoriteJokeId,
         onFavoriteTap: handleFavoriteTap,
       ),
+    );
+  }
+}
+
+class WelcomeScreen extends StatelessWidget {
+  final int? favoriteJokeId;
+  final Function(int) onFavoriteTap;
+
+  const WelcomeScreen({
+    super.key,
+    required this.favoriteJokeId,
+    required this.onFavoriteTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: jokes.length,
+      itemBuilder: (context, index) {
+        final joke = jokes[index];
+        final isFavorite = joke.id == favoriteJokeId;
+
+        return Container(
+          decoration: const BoxDecoration(
+            border: Border(
+              bottom: BorderSide(width: 0.5, color: Colors.grey),
+            ),
+          ),
+          padding: const EdgeInsets.fromLTRB(10, 20, 10, 20),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Expanded(
+                flex: 7,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      joke.title,
+                      style: TextStyle(
+                        color: appColor,
+                        fontWeight: FontWeight.w800,
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    Text(joke.description),
+                  ],
+                ),
+              ),
+              IconButton(
+                icon: Icon(
+                  isFavorite ? Icons.favorite : Icons.favorite_border,
+                  color: isFavorite ? Colors.red : Colors.grey,
+                ),
+                onPressed: () => onFavoriteTap(joke.id),
+              ),
+            ],
+          ),
+        );
+      },
     );
   }
 }
